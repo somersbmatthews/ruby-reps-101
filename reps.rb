b02b87f05367d1755a2c13379fc9e1b1be3d4164
James <code-for-coffee@users.noreply.github.com>
2016-11-07 11:12:09 -0600

    Update reps.rb

--- reps.rb
+++ reps.rb
@@ -2,3 +2,4 @@
 # makes sure to either `p my_output` or `puts my_output`.
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
+# to run, just `ruby reps.rb`
